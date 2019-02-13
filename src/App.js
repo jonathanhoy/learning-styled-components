12732ee1469d957acf0fb2ebbaedadd279f15c30
jonathanhoy <jonathanhoy5@gmail.com>
2019-02-12 21:04:12 -0500

    finished tutorial #4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Heading = styled.h1`
+  font-size: 2rem;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  border-radius: 4px;
+  color: white;
+  font-size: 2rem;
+  border: none;
+  /* background: indigo; */
+  /* ${props => props.type === 'cancel' && 'background: red;'}; */
+  background: ${props => (props.type === 'cancel' ? 'tomato;' : 'indigo;')};
+`;
 
 class App extends Component {
   render() {
@@ -8,9 +24,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <Heading>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </Heading>
+          <Button>Save</Button>
+          <Button type="cancel">Cancel</Button>
+          <Heading>Heading 2</Heading>
           <a
             className="App-link"
             href="https://reactjs.org"
