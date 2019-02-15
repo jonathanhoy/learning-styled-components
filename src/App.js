15f2f6c9c00a9c592cb6ef8cfdf6202638a26bb1
jonathanhoy <jonathanhoy5@gmail.com>
2019-02-14 20:59:16 -0500

    finished tutorial #6

--- src/App.js
+++ src/App.js
@@ -3,32 +3,60 @@ import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
+const Fake = ({ className }) => (
+  <div className={className}>
+    <h2>I'm a fake component</h2>
+  </div>
+)
+
 const Heading = styled.h1`
   font-size: 2rem;
 `;
 
+const color = "white";
+
 const Button = styled.button`
   padding: 5px 10px;
   border-radius: 4px;
-  color: white;
+  color: ${color};
   font-size: 2rem;
   border: none;
-  /* background: indigo; */
+  background: indigo;
   /* ${props => props.type === 'cancel' && 'background: red;'}; */
-  background: ${props => (props.type === 'cancel' ? 'tomato;' : 'indigo;')};
+  /* background: ${props => (props.type === 'cancel' ? 'tomato;' : 'indigo;')}; */
+`;
+
+const CancelButton = styled(Button)`
+  background: tomato;
+`
+
+const AppWrapper = styled.div`
+  header {
+    background: teal;
+  }
+  ${Button} {
+    margin-bottom: 2rem;
+  }
+`;
+
+const DoubleFake = styled(Fake)`
+  h2 {
+    color: red;
+  }
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppWrapper>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Heading>
             Edit <code>src/App.js</code> and save to reload.
           </Heading>
+          <DoubleFake />
           <Button>Save</Button>
-          <Button type="cancel">Cancel</Button>
+          <CancelButton>Cancel</CancelButton>
           <Heading>Heading 2</Heading>
           <a
             className="App-link"
@@ -39,7 +67,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
+      </AppWrapper>
     );
   }
 }
